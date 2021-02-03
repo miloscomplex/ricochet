31fe417c97d40ad5371be5343a55bfce2cfd855a
miloscomplex <hicallmesutton@gmail.com>
2021-02-03 01:42:48 -0600

    refactoring gamePlay.js and added matter.js to project

--- frontend/scripts/components/gamePlay.js
+++ frontend/scripts/components/gamePlay.js
@@ -9,19 +9,18 @@ class GamePlay {
       this.gameBall2 = new Ball(this.x ,30 ,this.ballRadius, 8, 1)
       this.gravity = new Vector(0, 0.4)
       this.wind = new Vector(0.2, 0)
-      this.platform = new Platform(canvas.width / 2, canvas.height / 2, 50, 8)
-      this.platform2 = new Platform(canvas.width / 4, canvas.height / 2, 40, 8)
+      //this.platform = new Platform(canvas.width / 2, canvas.height / 2, 50, 8)
+      //this.platform2 = new Platform(canvas.width / 4, canvas.height / 2, 40, 8)
       this.platforms = new Array()
       // lexical scoping needs the argument passed
-      canvas.addEventListener("click", event => this.logMousePos(event, this.platforms))
+      canvas.addEventListener("click", event => this.makePlatform(event, this.platforms))
       console.log(this.platforms);
     }
 
   drawCanvas = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     this.platforms.forEach( platform => platform.drawPlatform())
-    this.platform.drawPlatform()
-    this.platform2.drawPlatform()
+
     // update position
     this.gameBall.updatePosition()
     this.gameBall2.applyForce(this.gravity)
@@ -38,12 +37,8 @@ class GamePlay {
     this.drawCanvas()
   }
 
-  logMousePos = function(e, array) {
+   makePlatform = function(e, array) {
     array.push(new Platform(e.offsetX, e.offsetY, 50, 8))
-    //let newPlatform = new Platform(e.offsetX, e.offsetY, 80, 20)
-    //console.log(newPlatform);
-    console.log(array)
-    //this.platforms.push(platform)
   }
 
   // Why is this throw an error of not a function
