452d36d811a975fb2f90c371150344d149d410f2
miloscomplex <hicallmesutton@gmail.com>
2021-02-04 01:32:46 -0600

    tried to implement SVG

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -6,10 +6,22 @@ const ctx = canvas.getContext('2d')
 const Engine = Matter.Engine
 const World = Matter.World
 const Bodies = Matter.Bodies
+const Vertices = Matter.Vertices
+const Svg = Matter.Svg
 const engine = Engine.create()
 const world = engine.world
 world.gravity.y = 1.5
 
+// let loadSvg = function(url) {
+//   return fetch(url)
+//   .then(resp => resp.text())
+//   .then(raw => (new window.DOMParser()).parseFromString(raw, 'image/svg+xml'))
+// }
+//
+// (['../images:svgs/cup_simple.svg']).forEach(function(path, i) {
+//   let vertexSets = select(root, 'path')
+//   .map( path => Vertices.scale(Svg.pathToVertices(path, 30), 0.4, 0.4))
+// })
 // Initiate new game
 let newGame = new GamePlay()
 const interval = setInterval(newGame.draw, 10)
