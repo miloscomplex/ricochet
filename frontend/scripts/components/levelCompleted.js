197c7c0acbc87cd19500d2e760fc1ca267450f5f
miloscomplex <hicallmesutton@gmail.com>
2021-02-07 01:07:44 -0600

    buiding out levelCompleted.js

--- frontend/scripts/components/levelCompleted.js
+++ frontend/scripts/components/levelCompleted.js
@@ -1,17 +1,25 @@
 class LevelCompleted {
-  constructor(scoreInfo) {
+  constructor() {
     // need a clearing method
     canvasWrapper.innerHTML = '' //may be overkill
+    this.levelCompletedContainer()
+    this.successMessage()
+    this.nameForm()
+  }
 
-
+  levelCompletedContainer = function() {
     const levelCompletedContainer = document.createElement('div')
     levelCompletedContainer.id = 'levelCompletedContianer'
     canvasWrapper.append(levelCompletedContainer)
+  }
 
+  successMessage = function() {
     const successMessage = document.createElement('p')
     successMessage.innerText = `Excellent Job! You won in {scoreInfo.time} seconds and while using {scoreInfo.platforms_used} platforms. Submit your score to see how you rank.`
     levelCompletedContainer.append(successMessage)
+  }
 
+  nameForm = function() {
     const nameForm = document.createElement('form')
     levelCompletedContainer.append(nameForm)
     const nameLabel = document.createElement('label')
@@ -26,6 +34,5 @@ class LevelCompleted {
     submitButton.value = 'Submit Your Score'
 
     nameForm.append(nameLabel, nameInput, initialsLabel, initialsForm, submitButton)
-
   }
 }
